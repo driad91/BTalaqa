f8656817e56030e80a0b849c6091040e1a2a6abd
protvino <korzinovvv@gmail.com>
2019-03-11 21:32:10 +0100

    started dynamic forms
    added js and messages to the question_answer
    
    js is being loaded. forms cant be added yet
    
    dynamic forms ready
    
    gitignore with migrations

--- BTalaqa/BTalaqa/settings.py
+++ BTalaqa/BTalaqa/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +129,12 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL ='home'
 LOGOUT_REDIRECT_URL ='login'
 LOGIN_URL = 'login'
+
+# messages framework
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
